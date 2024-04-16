8e5aea3d0b64b79eb91b40233088a3f75a135980
AndreaDonato <donato.1808606@studenti.uniroma1.it>
2024-04-16 14:36:50 +0200

    Automatic Commit

--- fast_push.sh
+++ fast_push.sh
@@ -6,7 +6,7 @@ git push
 
 if [ $? -ne 0 ]; then
     # Se il codice di uscita non è zero, c'è stato un errore
-    echo "Errore durante il push al repository remoto. Si prega di controllare."
+    echo "Errore durante il push al repository remoto. Si prega di controllare manualmente."
     # Qui puoi aprire una finestra con il messaggio di errore, ad esempio utilizzando Zenity
-    zenity --error --text="Errore durante il push al repository remoto. Si prega di controllare."
+    zenity --error --text="Errore durante il push al repository remoto. Si prega di controllare manualmente."
 fi
